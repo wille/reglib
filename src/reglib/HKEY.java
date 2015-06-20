9d6e02932aede285d5b6fae844b6206ca4587f2a
redpois0n <red@cmail.nu>
2015-06-20 15:29:06 +0200

    HKEY class

---
+++ src/reglib/HKEY.java
@@ -0,0 +1,11 @@
+package reglib;
+
+public class HKEY {
+	
+	public static final int HKEY_CLASSES_ROOT = 0x80000000;
+	public static final int HKEY_CURRENT_CONFIG = 0x80000005;
+	public static final int HKEY_CURRENT_USER = 0x80000001;
+	public static final int HKEY_LOCAL_MACHINE = 0x80000002;
+	public static final int HKEY_USERS = 0x80000003;
+
+}
