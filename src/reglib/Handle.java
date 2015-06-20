9a8c8dda8d88db65d0f10443aa66e2307a2ff6c2
redpois0n <red@cmail.nu>
2015-06-20 15:39:49 +0200

    Handle class

---
+++ src/reglib/Handle.java
@@ -0,0 +1,15 @@
+package reglib;
+
+public class Handle {
+	
+	private final int handle;
+	
+	public Handle(int handle) {
+		this.handle = handle;
+	}
+	
+	public int getHandle() {
+		return this.handle;
+	}
+
+}
