ab6e2b427aac17a98d8e628b7d48d1a396cd75d4
redpois0n <red@cmail.nu>
2015-06-20 15:39:57 +0200

    openKey function

--- natives/registry.c
+++ natives/registry.c
@@ -1,1 +1,13 @@
 #include <windows.h>
+#include <jni.h>
+
+#include "reglib_NativeRegistry.h"
+#include "util.h"
+
+JNIEXPORT jobject JNICALL Java_reglib_NativeRegistry_openKey(JNIEnv * env, jclass z, jint hKey, jstring lpSubKey) {
+	int* ihandle;
+
+	RegOpenKeyEx(hKey, getcstring(env, lpSubKey), 0, 0, &ihandle);
+
+	return ihandle;
+}
--- src/reglib/NativeRegistry.java
+++ src/reglib/NativeRegistry.java
@@ -1,5 +1,7 @@
 package reglib;
 
 public class NativeRegistry {
+	
+	public static native Handle openKey(int hKey, String lpSubKey);
 
 }
