d5ae5e5eb204a69d9ba40f18e59e3bbfbe2de37a
redpois0n <red@cmail.nu>
2015-06-21 11:10:51 +0200

    handle removed

--- src/reglib/NativeRegistry.java
+++ src/reglib/NativeRegistry.java
@@ -1,9 +1,7 @@
 package reglib;
 
 public class NativeRegistry {
-	
-	public static native Handle openKey(int hKey, String lpSubKey);
-	
-	public static native Handle createKey(int hKey, String lpSubKey);
+		
+	public static native boolean createKey(int hKey, String lpSubKey);
 
 }
