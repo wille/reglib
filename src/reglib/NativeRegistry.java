4910596c2c6d20ebd2278373106ed87dff38fa9d
redpois0n <red@cmail.nu>
2015-06-24 22:56:09 +0200

    deleteKey() comment

--- src/reglib/NativeRegistry.java
+++ src/reglib/NativeRegistry.java
@@ -11,6 +11,13 @@ public class NativeRegistry {
 	 */
 	public static native int createKey(int hKey, String lpSubKey);
 	
+	/**
+	 * Returns the error code returned by <a href="https://msdn.microsoft.com/en-us/library/windows/desktop/ms724847(v=vs.85).aspx">RegDeleteKeyEx</a>
+	 * ERROR_SUCCESS (0x0) if succeeded or any of the system <a href="https://msdn.microsoft.com/en-us/library/windows/desktop/ms681382(v=vs.85).aspx">error codes</a> (0-499)
+	 * @param hKey {@link reglib.HKEY}
+	 * @param lpSubKey the subkey to delete
+	 * @return
+	 */
 	public static native int deleteKey(int hKey, String lpSubKey);
 
 }
